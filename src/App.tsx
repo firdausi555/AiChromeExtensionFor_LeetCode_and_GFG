d17d6cde672e8a7a58ae7b66cf8366f6cf7a6965
Sabil Firdausi <sabilfirdausi80@gmaail.com>
2024-11-17 01:10:09 +0530

    final fixes

--- src/App.tsx
+++ src/App.tsx
@@ -13,13 +13,13 @@ const Popup: React.FC = () => {
     ;(async function loadOpenAPIKey() {
       if (!chrome) return
       const apiKeyFromStorage = (await chrome.storage.local.get('apiKey')) as {
-        apiKey?: string;
-      };
+        apiKey?: string
+      }
       if (apiKeyFromStorage.apiKey)
-        setOpenAIKey(`${apiKeyFromStorage.apiKey.substring(0, 12)}-XXXXXX`);
-      setIsLoaded(true);
-    })();
-  }, []);
+        setOpenAIKey(`${apiKeyFromStorage.apiKey.substring(0, 12)}-XXXXXX`)
+      setIsLoaded(true)
+    })()
+  }, [])
 
   const handleAddOpenAPIKey = async () => {
     if (openAIKey) {
@@ -28,28 +28,20 @@ const Popup: React.FC = () => {
   }
 
   return (
-    <div className="dark relative w-[350px] bg-[#121627] p-4  text-black">
+    <div className="dark relative w-[350px] bg-[#4682B4] p-4  text-black">
       <Show show={isLoaded}>
         <div className="">
-          <div className="w-full  h-20 overflow-hidden ">
-            <img
-              className="mx-auto h-20 w-auto"
-              src={leetCode}
-              width={150}
-              height={150}
-            />
-          </div>
           <div className="text-center">
             <h1 className=" font-bold text-3xl text-white">
-              LeetCode <span className="text-whisperOrange">Whisper</span>
+              Your LeetCode/GFG <span>Assistant</span>
             </h1>
             <p className="text-base text-slate-400">
-              Your Companion to Beat LeetCode!
+              Your Own AI to help with those Cranky GFG/LeetCode Questions!
             </p>
           </div>
           <div className="mt-10 flex flex-col gap-2">
             <label htmlFor="text" className="text-white font-bold text-xl">
-              Enter Your OpenAI API key
+              Please Enter Your OpenAI API key
             </label>
             <Input
               value={openAIKey}
@@ -64,14 +56,8 @@ const Popup: React.FC = () => {
           </div>
           <div className=" h-16 flex items-center justify-center">
             <p className="text-white text-[14px]">
-              Want more features?&nbsp;
-              <a
-                href="https://github.com/piyushgarg-dev/leetcode-whisper-chrome-extension"
-                className="text-[#86ccee]"
-                target="_blank"
-              >
+              <a href="" className="text-[#86ccee]" target="_blank">
                 {' '}
-                Request a feature!
               </a>
             </p>
           </div>
